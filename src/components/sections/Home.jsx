6e7b957234bbcfa8ae278928dd27eb5d194db327
Issa Abu <168141352+Blackscorche@users.noreply.github.com>
2025-03-10 01:26:51 +0100

    Update Home.jsx

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -14,11 +14,11 @@ function Home() {
           </p>
 
           <div className="flex justify-center space-x-4">
-            <a href="/projects" className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]'>
+            <a href="#projects" className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]'>
               View Projects
             </a>
             
-            <a href="/contact" className='border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:bg-blue-500/10'>
+            <a href="#contact" className='border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:bg-blue-500/10'>
               Contact Me
             </a>
           </div>
